339e23dfc77f9204a91707f4e39eca751d368f1a
Sergio Patiño <sergioprodriguez20@gmail.com>
2025-12-11 22:21:33 -0500

    Agregar Barcos y Visual

--- src/main/java/com/example/batallanaval/Controllers/CanvasController.java
+++ src/main/java/com/example/batallanaval/Controllers/CanvasController.java
@@ -1,16 +1,35 @@
 package com.example.batallanaval.Controllers;
 
 
+import com.example.batallanaval.Models.BarcoModel;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.ImageCursor;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
 
 public class CanvasController {
 
-    private static final int cellSize =40;
+    private ArrayList<Canvas>Barcos = new ArrayList<>();
+
+    private static int cellSize =40;
+
+
+    Image Cursor = new Image(
+            getClass().getResourceAsStream("/com/example/batallanaval/CursorImage.png")
+    );
+
+
+    @FXML
+    private VBox VboxLeft;
 
     @FXML
     private AnchorPane AnchorPane;
@@ -18,48 +37,51 @@ public class CanvasController {
     @FXML
     private GridPane Grid;
 
+    @FXML
+    private GridPane Grid2;
 
 
-    public Canvas dibujarBarco(int longitud, boolean horizontal) {
-        // Usar tamaño fijo de celda basado en el GridPane
-
-        Canvas canvas;
-        if (horizontal) {
-            canvas = new Canvas(cellSize * longitud, cellSize);
-        } else {
-            canvas = new Canvas(cellSize, cellSize * longitud);
-        }
-
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-
-        gc.setFill(Color.GRAY);
-        gc.fillRoundRect(5, 5, canvas.getWidth() - 10, canvas.getHeight()-10, 10, 10);
-
-        gc.setStroke(Color.DARKGRAY);
-        gc.setLineWidth(2);
-        gc.strokeRoundRect(5, 5, canvas.getWidth() - 10, canvas.getHeight() - 10, 10, 10);
-
-        return canvas;
-    }
     @FXML
     public void initialize() {
-
-        // Ejecutar después de que se renderice la ventana
-        javafx.application.Platform.runLater(() -> {
-
-            Canvas barco1 = dibujarBarco(3,false);
-            barco1.setLayoutX(0 * cellSize);
-            barco1.setLayoutY(0 * cellSize);
-            AnchorPane.getChildren().add(barco1);
-
-            Canvas barco2 = dibujarBarco(2,true);
-            barco1.setLayoutX(1 * cellSize);
-            barco1.setLayoutY(3 * cellSize);
-            AnchorPane.getChildren().add(barco2);
-
-
-
-        });
+        IniciarBarcos();
     }
+
+    @FXML
+    void OnMouseEnteredGrid2(MouseEvent event) {
+        Grid2.setCursor(new ImageCursor(
+                Cursor,
+                Cursor.getWidth()/2,
+                Cursor.getHeight()/2
+        ));
+
+
+    }
+
+    public void IniciarBarcos(){
+
+        for (int i=1; i<5; i++){
+            Canvas canva = new BarcoModel(i,false).dibujarBarco();
+            Barcos.add(canva);
+        }
+
+        for (Canvas canvas:Barcos){
+            canvas.setCursor(javafx.scene.Cursor.HAND);
+            canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, e ->{
+                System.out.println("Barco presionado Presionado");
+            });
+            VboxLeft.getChildren().add(canvas);
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
 
 }
